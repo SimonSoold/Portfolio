ce037f2191f8f0603c971d440bc55a9d87b0e750
Simon Soold <soold.simon@gmail.com>
2021-08-31 06:44:10 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from "./components/about/about"
+import Contact from "./components/contact/contact"
+import Project from "./components/project/project"
+import backgroundPic from "./assets/backgroundPic.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router >
+      <div className="App">
+      <header><h1>Simon Soold</h1></header>
+      <div className="image"><img src={backgroundPic} alt=""></img></div>
+      <nav>
+        <ul>
+        <li>
+            <Link to="/about">Home</Link>
+          </li>
+          <li>
+            <Link to="/">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <Switch>
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/">
+            <Project></Project>
+          </Route>
+        </Switch>
+      </main>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/about/about.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const About = () => {
+    return (
+        <article className="about">
+            <h2>Lite info</h2>
+            <p>About</p>
+        </article>
+    )
+}
+
+export default About
